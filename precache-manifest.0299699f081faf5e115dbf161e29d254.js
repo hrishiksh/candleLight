4800f9c632524096d5f3ae783d790230cf17d572
hrishiksh <hrishi1pathak@gmail.com>
2020-06-13 11:32:35 +0530

    Updates

---
+++ precache-manifest.0299699f081faf5e115dbf161e29d254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79281bb6c44400fd5fecd892b397c88",
+    "url": "/candleLight/index.html"
+  },
+  {
+    "revision": "467654ffd93aaa6ab5b7",
+    "url": "/candleLight/static/css/main.16ac4bb8.chunk.css"
+  },
+  {
+    "revision": "ea8c85708b08983b7c03",
+    "url": "/candleLight/static/js/2.6e53ba15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/candleLight/static/js/2.6e53ba15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467654ffd93aaa6ab5b7",
+    "url": "/candleLight/static/js/main.43abd6c2.chunk.js"
+  },
+  {
+    "revision": "643af47d8e95cd678067",
+    "url": "/candleLight/static/js/runtime-main.b7f839bd.js"
+  }
+]);
